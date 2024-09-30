f4aaecb25cd420754d5c24a193e9335455dfcc5c
44David <david.soane@outlook.com>
2024-09-30 14:02:03 -0600

    Added authentication files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,15 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
+
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+        <main>
+          {children}
+        </main>
+             
       </body>
     </html>
   );
