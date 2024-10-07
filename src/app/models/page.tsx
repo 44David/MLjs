09810eb5f3f67d660de15fe96a1be97c1dcc3329
44David <david.soane@outlook.com>
2024-10-06 21:47:05 -0600

    added new page

---
+++ src/app/models/page.tsx
@@ -0,0 +1,6 @@
+export default function Models() {
+    return (
+       <h1>Explore and install models</h1> 
+    )
+    
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 export default function Home() {
+
   return (
     <div>
-      
+      <Link href={'models'}><Button className='block'>Pull a model</Button></Link>
+      <Button>Create/Modify a model</Button>
     </div>
   );
 }
